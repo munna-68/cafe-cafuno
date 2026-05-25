4a359b57a3956d6a8a09cfffe034aeb9ac647447
munna-68 <mahamudmunna68@gmail.com>
2026-05-25 20:43:17 +0600

    Refactor Navbar component for improved structure and styling; update link handling and active states

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,26 @@
-import { Link, useLocation } from "react-router-dom";
-import "./Navbar.css";
+import { Link, useLocation } from 'react-router-dom'
+import './Navbar.css'
 
 export default function Navbar() {
-  const { pathname } = useLocation();
-  const isMenu = pathname === "/menu";
-  const isReserve = pathname === "/reserve";
-  const isAtmosphere = pathname === "/atmosphere";
-  const isLocations = pathname === "/locations";
+  const { pathname } = useLocation()
+  const isHome = pathname === '/'
+  const isMenu = pathname === '/menu'
+  const isReserve = pathname === '/reserve'
+  const isAtmosphere = pathname === '/atmosphere'
+  const isLocations = pathname === '/locations'
 
   return (
     <nav className="navbar">
       <div className="navbar__inner">
+        <Link to="/" className="navbar__logo">CAFUNO</Link>
         <div className="navbar__links">
-          <a href="/#story" className="navbar__link">
-            STORY
-          </a>
-          <Link
-            to="/menu"
-            className={`navbar__link ${isMenu ? "navbar__link--active" : ""}`}
-          >
-            MENU
-          </Link>
-          <Link
-            to="/atmosphere"
-            className={`navbar__link ${isAtmosphere ? "navbar__link--active" : ""}`}
-          >
-            ATMOSPHERE
-          </Link>
-          <Link
-            to="/locations"
-            className={`navbar__link ${isLocations ? "navbar__link--active" : ""}`}
-          >
-            LOCATIONS
-          </Link>
+          <a href="/#story" className={`navbar__link ${isHome ? 'navbar__link--active' : ''}`}>STORY</a>
+          <Link to="/menu" className={`navbar__link ${isMenu ? 'navbar__link--active' : ''}`}>MENU</Link>
+          <Link to="/atmosphere" className={`navbar__link ${isAtmosphere ? 'navbar__link--active' : ''}`}>ATMOSPHERE</Link>
+          <Link to="/locations" className={`navbar__link ${isLocations ? 'navbar__link--active' : ''}`}>LOCATIONS</Link>
         </div>
-
-        <Link to="/" className="navbar__logo">
-          CAFUNO
-        </Link>
-
-        <Link
-          to="/reserve"
-          className={`navbar__cta ${isReserve ? "navbar__cta--active" : ""}`}
-        >
-          RESERVE
-        </Link>
+        <Link to="/reserve" className={`navbar__cta ${isReserve ? 'navbar__cta--active' : ''}`}>RESERVE</Link>
       </div>
     </nav>
-  );
+  )
 }
